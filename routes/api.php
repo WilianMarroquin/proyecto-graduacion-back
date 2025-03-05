da1eab84f965b7a991cc643309970db0242ddbfe
WilianMarroquin <githubwiliangit@gmail.com>
2025-03-04 19:25:28 -0600

    Implementación de la autenticación con laravel breeze

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
-Route::apiResource('users', App\Http\Controllers\Api\UserApiController::class)
-        ->parameters(['users' => 'user']);
+require __DIR__.'/auth.php';
