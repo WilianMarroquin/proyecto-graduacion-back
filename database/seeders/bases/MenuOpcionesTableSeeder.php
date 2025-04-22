bf10c8b379a48f7d117e20c1bd0c02cf56c55e1f
WilianMarroquin <githubwiliangit@gmail.com>
2025-04-21 19:31:52 -0600

    Modificación en el seeder para las opciones del menu.

--- database/seeders/bases/MenuOpcionesTableSeeder.php
+++ database/seeders/bases/MenuOpcionesTableSeeder.php
@@ -18,6 +18,7 @@ class MenuOpcionesTableSeeder extends Seeder
         MenuOpcion::truncate();
 
         MenuOpcion::create([
+            "id" => 1,
             "titulo" => "Inicio",
             "icono" => "ri-home-8-line",
             "ruta" => "index",
@@ -26,7 +27,9 @@ class MenuOpcionesTableSeeder extends Seeder
             "subject" => "Inicio",
             "parent_id" => null
         ]);
+
         MenuOpcion::create([
+            "id" => 2,
             "titulo" => null,
             "icono" => null,
             "ruta" => null,
@@ -36,7 +39,9 @@ class MenuOpcionesTableSeeder extends Seeder
             "subject" => "Configuracion",
             "parent_id" => null
         ]);
+
         MenuOpcion::create([
+            "id" => 3,
             "titulo" => "Modulo Usuarios",
             "icono" => "ri-group-line",
             "ruta" => "second-page",
@@ -45,97 +50,140 @@ class MenuOpcionesTableSeeder extends Seeder
             "subject" => "User",
             "parent_id" => null
         ]);
+
         MenuOpcion::create([
+            "id" => 4,
             "titulo" => "Usuarios",
             "icono" => "ri-list-ordered-2",
             "ruta" => "admin-modulo-usuarios-usuarios",
-            "orden" => 3,
+            "orden" => 0,
             "action" => "Listar usuarios",
             "subject" => "User",
             "parent_id" => 3
         ]);
+
         MenuOpcion::create([
+            "id" => 5,
             "titulo" => "Roles",
             "icono" => "ri-folder-shield-2-line",
             "ruta" => "admin-modulo-usuarios-roles",
-            "orden" => 4,
+            "orden" => 1,
             "action" => "Listar roles",
             "subject" => "Rol",
             "parent_id" => 3
         ]);
+
         MenuOpcion::create([
+            "id" => 6,
+            "titulo" => "Catálogos",
+            "icono" => "ri-folder-open-fill",
+            "ruta" => null,
+            "orden" => 2,
+            "action" => "Ver catálogos de usuario",
+            "subject" => "User",
+            "parent_id" => 3
+        ]);
+
+        MenuOpcion::create([
+            "id" => 7,
+            "titulo" => "Usuarios Estados",
+            "icono" => "ri-folder-user-fill",
+            "ruta" => "admin-modulo-usuarios-usuario-estados",
+            "orden" => 0,
+            "action" => "Listar estados de usuario",
+            "subject" => "Estado",
+            "parent_id" => 6
+        ]);
+
+        MenuOpcion::create([
+            "id" => 8,
             "titulo" => "Permisos",
             "icono" => "ri-file-shield-2-fill",
             "ruta" => "admin-modulo-usuarios-permisos",
-            "orden" => 5,
+            "orden" => 1,
             "action" => "Listar permisos",
             "subject" => "Permission",
-            "parent_id" => 3
+            "parent_id" => 6
         ]);
+
         MenuOpcion::create([
+            "id" => 9,
             "titulo" => "Configuraciones",
             "icono" => "ri-folder-settings-fill",
             "ruta" => null,
-            "orden" => 6,
+            "orden" => 3,
             "action" => "Ver modulo configuracion",
             "subject" => "Configuracion",
             "parent_id" => null
         ]);
+
         MenuOpcion::create([
+            "id" => 10,
             "titulo" => "Opciones Menu",
             "icono" => "ri-apps-2-add-line",
             "ruta" => "admin-configuraciones-menu",
-            "orden" => 7,
+            "orden" => 0,
             "action" => "Listar Menu Opciones",
             "subject" => "Menu Opcion",
-            "parent_id" => 7
+            "parent_id" => 9
         ]);
+
         MenuOpcion::create([
+            "id" => 11,
             "titulo" => "Generales",
             "icono" => "ri-settings-3-fill",
             "ruta" => "admin-configuraciones-generales",
-            "orden" => 8,
+            "orden" => 1,
             "action" => "Listar configuraciones generales",
-        "subject" => "Configuracion",
-            "parent_id" => 7
+            "subject" => "Configuracion",
+            "parent_id" => 9
         ]);
+
         MenuOpcion::create([
+            "id" => 12,
             "titulo" => null,
             "icono" => null,
             "ruta" => null,
-            "orden" => 9,
+            "orden" => 4,
             "titulo_seccion" => "Modulo Programación",
             "action" => "Ver Modulo Desarrollo",
             "subject" => "Desarrollo",
             "parent_id" => null
         ]);
+
         MenuOpcion::create([
+            "id" => 13,
             "titulo" => "Developers",
             "icono" => "ri-tools-fill",
             "ruta" => "second-page",
-            "orden" => 10,
+            "orden" => 5,
             "action" => "Ver Modulo Desarrollo",
             "subject" => "Desarrollo",
             "parent_id" => null
         ]);
+
         MenuOpcion::create([
+            "id" => 14,
             "titulo" => "Configuraciones",
             "icono" => "ri-settings-5-fill",
             "ruta" => "dev-configuraciones",
-            "orden" => 11,
+            "orden" => 0,
             "action" => "Listar Configuraciones",
             "subject" => "Configuracion",
-            "parent_id" => 11
+            "parent_id" => 13
         ]);
+
         MenuOpcion::create([
+            "id" => 15,
             "titulo" => "Componentes",
             "icono" => "ri-code-box-line",
             "ruta" => "dev-componentes",
-            "orden" => 12,
+            "orden" => 1,
             "action" => "Listar Componentes",
             "subject" => "Desarrollo",
-            "parent_id" => 11
+            "parent_id" => 13
         ]);
+
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
